976196d7a13afe27a7849bd0461234513daca38a
vivek <vivek28202028@gmail.com>
2026-05-20 02:21:29 +0530

    css box and javascript intro

---
+++ JavaScript/Intro.js
@@ -0,0 +1,6 @@
+// develope by branden deich by 1995 
+// first name mocha
+// register name ecma(europen computer manufacture association) script 
+// famous in 2014 - 15 when use as backend 2008
+// datatype are two types primitiv and non primitive in primitive string number 
+// and in non- primitive - data type  array object function 
